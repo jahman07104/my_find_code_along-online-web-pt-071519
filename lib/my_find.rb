fce32b27eecfc89000abe674062a6980b07afa4f
Patrick Harrison <jahman@bellatlantic.net>
2019-07-29 19:57:37 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -8,6 +8,6 @@ def my_find(collection)
   end
 end
 collection = (1..100).to_a
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }def my_find(collection)
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
 
 end
